a94701c40f9990d4c1b976ad80f45e8852f5857d
tseiman <t.schmidt@md-network.de>
2019-06-01 01:49:44 +0200

    Update testplugin.js

--- testplugin/testplugin.js
+++ testplugin/testplugin.js
@@ -4,6 +4,7 @@ const { testlib } = require('lib/testlib.js');
 class Testplugin {
 	
 	constructor(name, dom,append_to_id) {
+		console.log("running constructor of Testplugin");
 		var apped_to = (append_to_id === undefined) ? "#space" : append_to_id;
 		this.name = name;
 		var self = this;
