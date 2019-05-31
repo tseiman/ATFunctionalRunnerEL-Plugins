dd68e0507e3d16be5cf1fa895f838466a5440f8e
tseiman <t.schmidt@md-network.de>
2019-05-31 20:59:31 +0200

    Update testlib.js

--- plugins/testplugin/lib/testlib.js
+++ plugins/testplugin/lib/testlib.js
@@ -1,13 +1,10 @@
-
-class Table {
-	
+class Testlib {
 	constructor(testel) {
 		this.testel = testel;		
 	}
-	
-	someMethod(a,b) {
 
-    var res = a + b;
-    $(this.testel).append("<B>" + res + "</B>");
- 	}	
+	someMethod(a,b) {
+		var res = a + b;
+		$(this.testel).append("<B>" + res + "</B>");
+	}	
 }
