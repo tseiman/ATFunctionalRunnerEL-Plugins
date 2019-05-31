831aec170eddca91ef189b17ba7a12cb9a6fe0bc
tseiman <t.schmidt@md-network.de>
2019-05-31 20:58:12 +0200

    Create testplugin.js

---
+++ plugins/testplugin/testplugin.js
@@ -0,0 +1,17 @@
+
+const { testlib } = require('lib/testlib.js');
+
+class Testplugin {
+	
+	constructor(name, dom,append_to_id) {
+		var apped_to = (append_to_id === undefined) ? "#space" : append_to_id;
+		this.name = name;
+		var self = this;
+		
+		this.value = dom.evaluate( './/testplugin[@name="' + name + '"]', dom, null, XPathResult.STRING_TYPE, null ).stringValue;
+
+		new Testlib(apped_to);
+    		
+	}
+	
+}
