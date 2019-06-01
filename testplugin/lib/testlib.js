cc0003d6ab3313fbcc3b353b3bf7bf7f0eeff58a
Thomas Schmidt <t.schmidt@md-network.de>
2019-06-01 22:57:58 +0200

    added event handling

--- testplugin/lib/testlib.js
+++ testplugin/lib/testlib.js
@@ -1,12 +1,21 @@
 class Testlib {
-	constructor(testel,name) {
+
+	constructor(testel,name,callback) {
 		this.testel = testel;		
 		this.name = name;
+		this.callback = callback;
+		console.log("calling constructor of testlib with name: " +  name);
 	}
 
 	someMethod(a,b) {
+		var self = this;
 		var res = a + b;
-		$(this.testel).append("<B>Hello World! Name is:" + this.name + " Res:" + res + "</B>");
+		$(this.testel).append("<div id='testlib-" + this.name + "'><B>Hello World! Name is:" + this.name + " Res:" + res + "</B><div>");
+		$( "#testlib-" + this.name ).click(function() {
+		    if(self.callback !== undefined && self.callback !== null) {
+			self.callback(self.name);
+		    }		
+		});
 	}	
 }
 module.exports = Testlib;
--- testplugin/testplugin.js
+++ testplugin/testplugin.js
@@ -6,16 +6,20 @@ console.log("Plugina Class read");
 class Testplugin {
 	
 	constructor(name, dom,append_to_id) {
-		console.log("running constructor of Testplugin");
+		console.log("running constructor of Testplugin name:" + name );
 		var apped_to = (append_to_id === undefined) ? "#space" : append_to_id;
 		this.name = name;
 		var self = this;
 		
 		this.value = dom.evaluate( './/testplugin[@name="' + name + '"]', dom, null, XPathResult.STRING_TYPE, null ).stringValue;
 
-		var myLib = new Testlib(apped_to,this.name);
+		var myLib = new Testlib(apped_to,this.name,this.callback);
 		myLib.someMethod(1,2);
     		
+	}
+	
+	callback(name) {
+	    console.log("clicked: " + name);
 	}
 	
 }
